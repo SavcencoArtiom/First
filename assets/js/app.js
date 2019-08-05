019c170098165a8171c15016c8dd473af0c36c85
artiom <artem@MacBook-Pro-Artem.local>
2019-08-05 15:03:47 +0300

    PSD-06

--- assets/js/app.js
+++ assets/js/app.js
@@ -37,27 +37,36 @@ $(function () {
             scrollTop: blockOffset
         }, 500)
 
-
-        // Menu nav toggle
-        $("#nav_toggle").on("click", function (event) {
-            event.preventDefault();
-
-            $(this).toggleClass("active");
-            $("#nav").toggleClass("active");
-        });
-
-
-
-
-
-
     });
 
 
+    // Menu nav toggle
+    $("#nav_toggle").on("click", function (event) {
+        event.preventDefault();
+
+        $(this).toggleClass("active");
+        $("#nav").toggleClass("active");
+    });
+
+    // Collapse
+
+    $("[data-collapse]").on("click", function (event) {
+        event.preventDefault();
+
+        var $this = $(this),
+            blockId = $this.data("collapse");
+
+        $this.toggleClass("active");
+    });
 
 
-
-
+    // Slieder
+    $("[data-slider]").slick({
+        infinite: true,
+        fade: false,
+        slidesToShow: 1,
+        sledesToScroll: 1
+    });
 
 
 
